d1bd959f4ebe44ef269912625afbe3e3c82bcd9c
evfairchild <evfairchild@gmail.com>
2020-04-17 09:47:49 -0700

    initial commit, client and server websocket working locally

---
+++ client.py
@@ -0,0 +1,33 @@
+__author__ = "evfairchild"
+
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(msg)
+
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+
+    client.send(send_length)
+    client.send(message)
+
+    print(client.recv(2048).decode(FORMAT))     # receive message from server
+
+
+send(input("Send to Server: "))
+
+send(DISCONNECT_MESSAGE)
+
---
+++ server.py
@@ -0,0 +1,55 @@
+__author__ = "evfairchild"
+
+import socket
+import threading
+
+HEADER = 64     # bytes
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        # blocking line, will not continue until something is received form the client
+
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            # message protocol: first message will always be of length HEADER.
+            # The client will respond with the length of the incoming message
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))    # send message to the client
+
+    conn.close()
+
+
+def start():
+    """Handles new connections"""
+    server.listen()
+    print(f"[LISTENING] server is listening on {SERVER}")
+
+    while True:
+        conn, addr = server.accept()    # blocking line, will not continue until a new connection is made
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
